05e8dcde93c50e82bde8b84e11f6c698a630e593
EricksonLopes <ofc.erickson@gmail.com>
2020-05-21 14:42:57 -0300

    aula 29

--- Cursos Python/Programação web com django/core/admin.py
+++ Cursos Python/Programação web com django/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cliente, Produto
 
-# Register your models here.
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque')
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'email')
+
+
+admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Cliente, ClienteAdmin)
--- Cursos Python/Programação web com django/core/models.py
+++ Cursos Python/Programação web com django/core/models.py
@@ -3,5 +3,17 @@ from django.db import models
 
 class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    preco = models.DecimalField('Preço', )
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque = models.IntegerField('Quantidade em estoque')
+
+    def __str__(self):
+        return self.nome
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome ', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
+
+    def __str__(self):
+        return self.nome
