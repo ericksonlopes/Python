c7d30de40e736f9cd543aef63c2e704d0835e0ed
EricksonLopes <ofc.erickson@gmail.com>
2020-05-21 15:55:16 -0300

    aula 30, django

--- Cursos Python/Programação web com django/core/views.py
+++ Cursos Python/Programação web com django/core/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuario não esta logado'
+    else:
+        teste = 'Usuario logado'
+
     context = {"curso": "Reprogramação",
-               "outro": 'programação'}
+               "outro": 'programação',
+               'user': teste}
     return render(request, 'index.html', context)
 
 
