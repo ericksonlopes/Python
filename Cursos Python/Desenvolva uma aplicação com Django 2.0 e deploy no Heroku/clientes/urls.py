c4ebdeebb82e6021d61898e6ec0048e19e5a1bf3
EricksonLopes <ofc.erickson@gmail.com>
2020-05-28 10:33:49 -0300

    18 - crud create

--- Cursos Python/Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/clientes/urls.py
+++ Cursos Python/Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/clientes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import person_list
-
+from .views import person_new
 
 urlpatterns = [
-    path('list/', person_list),
+    path('list/', person_list, name='person_list'),
+    path('new/', person_new, name='person_new'),
     ]
--- Cursos Python/Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/clientes/views.py
+++ Cursos Python/Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/clientes/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Person
+from .forms import PersonForm
 
 def person_list(request):
     persons = Person.objects.all()
     return render(request, 'person.html', {"persons": persons})
+
+def person_new(request):
+    form = PersonForm(request.POST, request.FILES, None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+    return render(request, 'person_form.html', {'form': form})
