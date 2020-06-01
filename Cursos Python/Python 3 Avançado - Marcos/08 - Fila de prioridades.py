583bb7785f9a6a42da0b1265d358ad592dcb47f4
EricksonLopes <ofc.erickson@gmail.com>
2020-06-01 10:24:05 -0300

    python avançado

---
+++ Cursos Python/Python 3 Avançado - Marcos/08 - Fila de prioridades.py
@@ -0,0 +1,35 @@
+import heapq
+
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    # adiciona item na lista
+    def push(self, item, priority):
+        # -priority ( para fica rdo maior para o menor)
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    # remove um item da lista
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+
+q = PriorityQueue()
+
+q.push(Person('Erickson'), 19)
+q.push(Person('joao'), 30)
+q.push(Person('calaoira'), 17)
+
+# retorna o a pessoa com maior número na idade
+print(q.pop())
