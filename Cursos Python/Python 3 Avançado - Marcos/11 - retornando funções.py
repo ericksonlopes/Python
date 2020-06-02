c1fada7f86b9b3bd81d9d332edecb6ae130cb1e3
EricksonLopes <ofc.erickson@gmail.com>
2020-06-02 14:41:58 -0300

    aula 11

---
+++ Cursos Python/Python 3 Avançado - Marcos/11 - retornando funções.py
@@ -0,0 +1,16 @@
+def pai(num):
+    def filho1():
+        print('Sou o filho 1, é igual a 10')
+
+    def filho2():
+        print('Sou o filho 2, não é igual a 10')
+
+    try:
+        assert num == 10
+        return filho1()
+    except AssertionError:
+        return filho2()
+
+
+pai(11)
+
