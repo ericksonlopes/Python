c8bef500f2d3e4a63d850f96dfa219eff2ecebda
EricksonLopes <ofc.erickson@gmail.com>
2020-06-11 11:22:24 -0300

    deploy

--- Cursos Python/Desenvolva uma aplicação com Django 2.0 e deploy no Heroku/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!E:\Compartilhamento de Informações\Git-hub\Python\Cursos Python\Desenvolva uma aplicação com Django 2.0 e deploy no Heroku\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
