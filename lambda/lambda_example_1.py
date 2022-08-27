20dadcab19cfcbd3b2cc590a9fb79036f1b02688
Erickson Lopes <ofc.erickson@gmail.com>
2022-08-27 11:20:38 -0300

    add lamba

---
+++ lambda/lambda_example_1.py
@@ -0,0 +1,24 @@
+# todo: Sum with lambda
+sum1 = lambda x, y: x + y
+
+print(sum1(1, 2))
+# Output
+# 3
+
+
+# todo: compare values
+# if x > y: result = x or y
+compare = (lambda x, y: x if x > y else y)
+print(compare(2, 9))
+# Output
+# 9
+
+# todo: Surrounding the function and its arguments with parentheses:
+print((lambda x, y: x + y)(1, 2))
+# Output
+# 3
+
+person = {'name': 'Ana', 'age': 23}
+print((lambda x: f"My name is {person['name']}, and I am {person['age']} years old")(person))
+# Output
+# My name is Ana, and I am 23 years old
