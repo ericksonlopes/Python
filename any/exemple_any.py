30ca3f538f3c80db4aa96d534132f94c65a5011b
Erickson Lopes <ofc.erickson@gmail.com>
2023-04-16 15:30:53 -0300

    exemple_any.py

---
+++ any/exemple_any.py
@@ -0,0 +1,9 @@
+list_zero = [0, 0, 0, 0, 0]
+
+if not any(list_zero):
+    print('There is no zero number in the list')
+
+list_num = [0, 0, 1, 0, 0]
+
+if any(list_num):
+    print('There is at least one non-zero number in the list')
