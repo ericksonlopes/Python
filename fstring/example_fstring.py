f7b107e75beed50b1324905eca48ad87ef70115d
Erickson Lopes <ofc.erickson@gmail.com>
2023-04-30 12:17:26 -0300

    example_fstring.py

--- fstring/example_fstring.py
+++ fstring/example_fstring.py
@@ -1,5 +1,16 @@
 print(f"{1+1=}")
+# 1+1=2
 
 text = "Hello"
 print(f"{text:#>10}")
+# ####Hello
+
 print(f"{text:_<10}")
+# Hello_____
+
+dicio = {"nome": "João", "idade": 30}
+print(f"{dicio=}")
+# dicio={'nome': 'João', 'idade': 30}
+
+print(f"{dicio['nome']=}")
+# dicio['nome']='João'
